3811da88c74006234467c4082684a895749302ca
masa-tk37 <masa.tk37@gmail.com>
2026-04-25 18:54:08 +0900

    feat: add umeboshifont cask

---
+++ Casks/font-umeboshifont.rb
@@ -0,0 +1,10 @@
+cask 'font-umeboshifont' do
+  version '2026.04.16'
+  sha256 '06865cf0a63180b676005088087a5a9a2fb469bf66b32d4711ff6da272d5785d'
+
+  url "https://font.xxenxx.net/zip/umeboshifont.zip"
+  name '梅干フォント'
+  homepage 'https://font.xxenxx.net/umeboshifont.html'
+
+  font 'umeboshi.ttf'
+end
