6f4ec20ac014e022421b7718c0787fe9f4aebf39
masa-tk37 <masa.tk37@gmail.com>
2026-05-03 17:13:22 +0900

    feat: upgrade azukifont to https and add uzurafont

---
+++ Casks/font-uzurafont.rb
@@ -0,0 +1,10 @@
+cask 'font-uzurafont' do
+  version '1.00'
+  sha256 '0cc1589532bfd27d509af4d601b3d3d38fd2b50d8a16b625e9f6310973d19af3'
+
+  url "https://azukifont.com/font/uzurafont100.zip"
+  name 'うずらフォント'
+  homepage 'https://azukifont.com/font/uzura.html'
+
+  font 'uzurafont100/uzura.ttf'
+end
